25168d001906d82dddcaf74e1f936ff2167eb643
L-Law-Liet <a.kadirov.17.06@gmail.com>
2021-10-19 12:21:09 +0500

    auth with sanctum

---
+++ app/Modules/Auth/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        $token = $user->createToken('auth')->plainTextToken;
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
