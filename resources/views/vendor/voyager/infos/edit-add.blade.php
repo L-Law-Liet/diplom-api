78e19bb3e4afa14f9cc0dacd02c0a671e8b6f0e7
L-Law-Liet <a.kadirov.17.06@gmail.com>
2022-03-19 13:20:34 +0500

    customize voyager

--- resources/views/vendor/voyager/infos/edit-add.blade.php
+++ resources/views/vendor/voyager/infos/edit-add.blade.php
@@ -1,29 +1,34 @@
 @extends('voyager::bread.edit-add')
 @section('submit-buttons')
-    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=7996e89b-a008-42d6-9408-258858c8afe3" type="text/javascript"></script>
-    <style>
-    </style>
-    <div id="map" style="width: 400px;height: 400px"></div>
-    <script>
-        ymaps.ready(function () {
-            let myMap = new ymaps.Map('map', {
-                    center: [55.751574, 37.573856],
-                    zoom: 9
-                }),
-                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-                    hintContent: 'Собственный значок метки',
-                    balloonContent: 'Это красивая метка'
+    @if($dataTypeContent->key == 'xy')
+        <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=7996e89b-a008-42d6-9408-258858c8afe3" type="text/javascript"></script>
+        <div id="map" style="width: 100%;height: 400px"></div>
+        <script>
+            let el = document.getElementsByName('value')[0]
+            el.style.display = 'none'
+            ymaps.ready(function () {
+                let coords = JSON.parse(el.value)
+                console.log(coords)
+                let myMap = new ymaps.Map('map', {
+                        center: coords,
+                        zoom: 9
+                    }),
+                    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                        hintContent: 'Собственный значок метки',
+                        balloonContent: 'Это красивая метка'
+                    });
+
+                myMap.geoObjects
+                    .add(myPlacemark);
+
+                myMap.events.add('click', function (e) {
+                    // Получение координат щелчка
+                    coords = e.get('coords');
+                    el.value = JSON.stringify(coords)
+                    myPlacemark.geometry.setCoordinates(coords);
                 });
-
-            myMap.geoObjects
-                .add(myPlacemark);
-
-            myMap.events.add('click', function (e) {
-                // Получение координат щелчка
-                let coords = e.get('coords');
-                myPlacemark.geometry.setCoordinates(coords);
             });
-        });
-    </script>
-    <button type="submit" class="btn btn-primary save">Save And Publish</button>
+        </script>
+    @endif
+    <button type="submit" class="btn btn-primary save" style="margin: 1rem 0;">Save And Publish</button>
 @endsection
