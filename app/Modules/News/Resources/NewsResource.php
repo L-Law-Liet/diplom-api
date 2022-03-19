c2fb1c6b185801de35d930af4de8748d3ad2b7b6
L-Law-Liet <a.kadirov.17.06@gmail.com>
2022-03-19 13:24:11 +0500

    add resources

---
+++ app/Modules/News/Resources/NewsResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\News\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $data = parent::toArray($request);
+        $data['image'] = getLink($this->image);
+        return $data;
+    }
+}
